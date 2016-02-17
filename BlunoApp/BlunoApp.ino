1daf59164fe101122d926195908c27702cab05a3
Forgeling <benoit.renault@insa-lyon.fr>
2016-02-17 23:34:51 +0100

    Declared all possible states, defined and tested the first ones

--- BlunoApp/BlunoApp.ino
+++ BlunoApp/BlunoApp.ino
@@ -2,6 +2,11 @@
 #include <Keypad.h>
 
 //------------------------------------------------------------------------------------------
+// Default Config
+//------------------------------------------------------------------------------------------
+const String CONFIG_CODE = "1337";
+
+//------------------------------------------------------------------------------------------
 // LCD Configuration and methods
 //------------------------------------------------------------------------------------------
 
@@ -17,6 +22,9 @@ int adc_key_in  = 0;
 #define btnLEFT   3
 #define btnSELECT 4
 #define btnNONE   5
+
+#define LCD_LINE_LENGTH 16
+#define LCD_NB_LINES 2
  
 void read_LCD_buttons(){               // read the buttons
     adc_key_in = analogRead(0);       // read the value from the sensor 
@@ -28,11 +36,16 @@ void read_LCD_buttons(){               // read the buttons
     if (adc_key_in > 1000) lcd_key = btnNONE; 
  
     // For V1.1 us this threshold
-    if (adc_key_in < 50)   lcd_key = btnRIGHT;  
-    if (adc_key_in < 250)  lcd_key = btnUP; 
-    if (adc_key_in < 450)  lcd_key = btnDOWN; 
-    if (adc_key_in < 650)  lcd_key = btnLEFT; 
     if (adc_key_in < 850)  lcd_key = btnSELECT;
+    if (adc_key_in < 650)  lcd_key = btnLEFT; 
+    if (adc_key_in < 450)  lcd_key = btnDOWN; 
+    if (adc_key_in < 250)  lcd_key = btnUP; 
+    if (adc_key_in < 50)   lcd_key = btnRIGHT;     
+
+   lcd.setCursor(9,1);             // move cursor to second line "1" and 9 spaces over
+   lcd.print(millis()/1000);       // display seconds elapsed since power-up
+ 
+   lcd.setCursor(0,1);             // move to the begining of the second line
     
    // depending on which button was pushed, we perform an action
    switch (lcd_key){
@@ -55,10 +68,6 @@ void read_LCD_buttons(){               // read the buttons
        }
        case btnSELECT:{
             lcd.print("SELECT");  //  push button "SELECT" and show the word on the screen
-            break;
-       }
-       case btnNONE:{
-            lcd.print("NONE  ");  //  No action  will show "None" on the screen
             break;
        }
    }
@@ -105,30 +114,186 @@ byte colPins[numCols]= {16,17,18,19}; //Columns 0 to 3
 //initializes an instance of the Keypad class
 Keypad myKeypad= Keypad(makeKeymap(keymap), rowPins, colPins, numRows, numCols);
 
-void readPad ()
+char readPad ()
 {
-  char keypressed = myKeypad.getKey();
-  if (keypressed != NO_KEY)
+  char keyPressed;
+  while ((keyPressed = myKeypad.getKey()) == NO_KEY){} // As long as the read key is nothing, read again.
+  return keyPressed; // Return the pressed char
+}
+
+String readCode (int expectedLength = 0)
+{
+  if (expectedLength == 0) expectedLength = LCD_LINE_LENGTH;
+  
+  String theCode = "";
+  char keyPressed;
+  int charCounter = 0;
+  
+  while((keyPressed = readPad()) != 'D' && charCounter < expectedLength)
   {
-  Serial.print(keypressed);
+    lcd.setCursor(0,1);
+    lcd.print("                ");
+    lcd.setCursor(0,1);
+    if (keyPressed == 'C')
+    {
+      charCounter--;
+      theCode.remove(charCounter);
+      lcd.print(theCode);
+    }
+    else
+    {
+      theCode += keyPressed;
+      lcd.print(theCode);
+      charCounter++;
+    }
+  }
+  Serial.println("Returning the code !");
+  return theCode;
+}
+
+//------------------------------------------------------------------------------------------
+// Displays
+//------------------------------------------------------------------------------------------
+#define ENTER_CONFIG_CODE_DISPLAY 0
+#define MANUAL_OR_BLUETOOTH_CONFIG_DISPLAY 1
+#define BLUETOOTH_CONFIG_DISPLAY 2
+#define GAME_MODE_CONFIG_DISPLAY 3
+#define GAME_DURATION_CONFIG_DISPLAY 4
+#define TIME_FOR_DEFUSING_CONFIG_DISPLAY 5
+#define KEY_CODE_CONFIG_DISPLAY 6
+#define CODE_LENGTH_CONFIG_DISPLAY 7
+#define CODE_DEFINITION_CONFIG_DISPLAY 8
+#define READY_TO_START_DISPLAY 9
+#define WAITING_FOR_CODE_DISPLAY 10
+#define WAITING_FOR_KEY_DISPLAY 11
+#define WAITING_FOR_BOTH_DISPLAY 12
+#define BOX_OPENED_DISPLAY 13
+#define END_OF_GAME_DISPLAY 14
+
+// Errors
+#define WRONG_CODE_LENGTH_DISPLAY 101
+#define WRONG_CODE_DISPLAY 102
+#define WRONG_DURATION_VALUE 103
+
+// Variable
+int currentDisplay = ENTER_CONFIG_CODE_DISPLAY;
+int previousDisplay = currentDisplay;
+
+//----------------------------- Config Code Display ----------------------------------------
+int enterConfigCodeDisplay ()
+{
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("Config code :");
+
+  if (readCode() == CONFIG_CODE)
+  {
+    return 1;
+  }
+  else
+  {
+    return 0;
   }
 }
 
+//----------------------------- Config Manual/Bluetooth ------------------------------------
+int manualOrBluetoothDisplay ()
+{
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("Manual (1)");
+  lcd.setCursor(0,1);
+  lcd.print("Bluetooth (2)");
+
+  char keyPressed;
+  while (true)
+  {
+    keyPressed = readPad();
+    if (keyPressed == '1') return 1;
+    else if (keyPressed == '2') return 2;
+  }
+}
+
+//----------------------------- Bluetooth config Display -----------------------------------
+
+//----------------------------- Game mode config Display -----------------------------------
+
+//----------------------------- Game duration config Display -------------------------------
+
+//----------------------------- Time for defusing config Display ---------------------------
+
+//----------------------------- Key/Code config Display ------------------------------------
+
+//----------------------------- Code length config Display ---------------------------------
+
+//----------------------------- Code definition config Display -----------------------------
+
+//----------------------------- Ready to start Display -------------------------------------
+
+//----------------------------- Waiting for code Display -----------------------------------
+
+//----------------------------- Waiting for key Display ------------------------------------
+
+//----------------------------- Waiting for both Display -----------------------------------
+
+//----------------------------- Box opened ! Display ---------------------------------------
+
+//----------------------------- End of game Display ----------------------------------------
+
+//----------------------------- Wrong code length Display ----------------------------------
+
+//----------------------------- Wrong code Display -----------------------------------------
+void wrongCodeDisplay ()
+{
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("Wrong code !");
+  delay (1000);
+}
+
+//----------------------------- Wrong duration value Display -------------------------------
 
 void setup(){
    Serial.begin(115200);  //initial the Serial (for Bluetooth connection)
    
    pinMode(KEY_PIN, INPUT); // Set the key pin as input
    
-   lcd.begin(16, 2);               // start the library
+   lcd.begin(LCD_LINE_LENGTH, LCD_NB_LINES);               // start the library
    lcd.setCursor(0,0);             // set the LCD cursor   position 
    lcd.print("Push the buttons");  // print a simple message on the LCD
 }
-  
+
+
+int theBuffer;
+
 void loop(){
-   // Read the strings that are sent via Bluetooth
-   if (Serial.available())  {
-    command = Serial.readString();
-   }
-   read_LCD_buttons();
+  // Read the strings that are sent via Bluetooth
+  //if (Serial.available())  {
+  // command = Serial.readString();
+  //}
+  
+  switch (currentDisplay)
+  {
+    // General cases
+    case ENTER_CONFIG_CODE_DISPLAY :
+      theBuffer = enterConfigCodeDisplay();
+      if (theBuffer == 1) currentDisplay = MANUAL_OR_BLUETOOTH_CONFIG_DISPLAY;
+      else
+      {
+        previousDisplay = currentDisplay;
+        currentDisplay = WRONG_CODE_DISPLAY;
+      }
+      break;
+    case MANUAL_OR_BLUETOOTH_CONFIG_DISPLAY :
+      theBuffer = manualOrBluetoothDisplay();
+      if (theBuffer == 2) currentDisplay = BLUETOOTH_CONFIG_DISPLAY;
+      else currentDisplay = GAME_MODE_CONFIG_DISPLAY;
+      break;
+      
+    // Error cases
+    case WRONG_CODE_DISPLAY :
+      wrongCodeDisplay();
+      currentDisplay = previousDisplay;
+      break;
+  }
 }
---
+++ CDC_config_malette.md
@@ -0,0 +1,32 @@
+# Differentes propriétés possible :
+
+  -activer désamorçage par clé.
+  -activer désamorçage par code.
+    -taille du mdp (mot de passe).
+    -mdp.
+      -> possibilité d'activer les deux modes de desamorçage.
+  -saisie durée de la partie.
+  -activer malus de temps si erreur (retire 5-7.5-10-12.5-15% du temps restant a chaque erreur a la saisie du code).
+
+## Ingame displays :
+
+### Code display :
+    05:32, Code:
+
+    ................
+
+### Key or code display :
+    05:32, Key/Code:
+
+    ................
+
+### Key display :
+    05:32, Turn the
+
+    key now !......
+
+### Key and code display :
+Display key, then code.
+
+### Code and key display :
+Display code then key.
